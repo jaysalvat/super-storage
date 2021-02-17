33a7d49b082405b224bf10fe31cca4fc1c22fb45
Jay Salvat <jay@jaysalvat.com>
2021-02-17 15:59:29 +0100

    Update Rollup with banners

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
+const watched = process.env.ROLLUP_WATCH
 const NAME = 'superStorage'
 const FILENAME = 'super-storage'
 const SRC = './src'
@@ -7,6 +9,23 @@ const DIST = './build'
 
 const configs = []
 const formats = [ 'esm', 'esm.min', 'umd', 'umd.min' ]
+
+const date = new Date()
+
+const bannerFull = `
+/**!
+* ${NAME} — ${pkg.description}
+* https://github.com/jaysalvat/super-store
+* @version ${pkg.version} built ${date.toISOString().replace(/[TZ]/g, ' ')}
+* @license MIT
+* @author Jay Salvat http://jaysalvat.com
+*/`
+
+const bannerLight = `
+/*!
+ * ${NAME} v${pkg.version}
+ * https://github.com/jaysalvat/super-store
+ */`
 
 formats.forEach((type) => {
   const [ format, minify ] = type.split('.')
@@ -19,8 +38,9 @@ formats.forEach((type) => {
       format: format,
       file: DIST + '/' + filename,
       name: format === 'umd' ? NAME : null,
+      banner: !watched && (minify ? bannerLight : bannerFull),
       plugins: [
-        minify ? terser() : null
+        !watched && minify ? terser() : null
       ]
     }
   })
